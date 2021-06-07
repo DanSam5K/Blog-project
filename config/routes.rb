6186a8d9906851d4d5aeb7f108861e1b2e198c01
Daniel Samuel <dansam5000@gmail.com>
2021-06-07 11:18:24 +0100

    Refactor code with resourceful routing

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,9 @@
 Rails.application.routes.draw do
   root "articles#index"
 
-  get "/articles", to: "articles#index"
-  get "/articles/:id", to: "articles#show"
+  # resource routing
+  resources :articles
+  # get "/articles", to: "articles#index"
+  # get "/articles/:id", to: "articles#show"
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
