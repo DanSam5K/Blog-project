d45a97a15ab8922b0c8410beb53a57cdfe132beb
Daniel Samuel <dansam5000@gmail.com>
2021-06-07 14:36:53 +0100

    Add validation and update error message

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -15,7 +15,7 @@ class ArticlesController < ApplicationController
   end
 
   def create
-    @article = Article.new(title: "...", body: "...")
+    @article = Article.new(article_params)
 
     if @article.save
       redirect_to @article
